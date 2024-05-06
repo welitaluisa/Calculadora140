75810aa8cdd07bb4d4624306deaec1b1afa04a69
welitaluisa <welitaluisa@gmail.com>
2024-05-06 16:07:36 -0300

    Calculadora em Python

--- __tests__/test_calculadora.py
+++ __tests__/test_calculadora.py
@@ -22,4 +22,41 @@ def test_somar_dois_numeros():
 
     assert resultado_esperado == resultado_obtido
 
+def test_subtrair_dois_numeros():
 
+    num1 = 7
+    num2 = 5 
+    resultado_esperado = 2
+
+    # Act / Executa
+    resultado_obtido = subtrair_dois_numeros(num1, num2)
+
+    # Assert / Valida
+
+    assert resultado_esperado == resultado_obtido
+
+def test_dividir_dois_numeros():
+
+    num1 = 8
+    num2 = 4 
+    resultado_esperado = 2
+
+    # Act / Executa
+    resultado_obtido = dividir_dois_numeros(num1, num2)
+
+    # Assert / Valida
+
+    assert resultado_esperado == resultado_obtido
+
+def test_multiplicar_dois_numeros():
+
+    num1 = 2
+    num2 = 4 
+    resultado_esperado = 8
+
+    # Act / Executa
+    resultado_obtido = multiplicar_dois_numeros(num1, num2)
+
+    # Assert / Valida
+
+    assert resultado_esperado == resultado_obtido    
