847e1b5f467717d84f7be554ae1d120cfe2fccb2
welitaluisa <welitaluisa@gmail.com>
2024-05-06 19:56:03 -0300

    Calculadora em Python usando massa de teste

--- __tests__/test_calculadora.py
+++ __tests__/test_calculadora.py
@@ -2,6 +2,7 @@
 import pytest
 
 from calculadora.calculadora import somar_dois_numeros, subtrair_dois_numeros, dividir_dois_numeros, multiplicar_dois_numeros
+from utils.utils import ler_csv
 
 # 2 - Testes
 
@@ -73,4 +74,42 @@ def test_dividir_por_zero():
     # Assert / Valida
 
     assert resultado_esperado == resultado_obtido
+
+# Test Baseados em Dados = Data Driven Tests (DDT) --> Massa de Testes
+    # Dados em uma lista
+    # dados em um arquivo, varios formatos: csv ( texto separado por virgulas), json , xml, dat
+@pytest.mark.parametrize('num1, num2, resultado_esperado', 
+                            [ # array / matriz
+                                (5, 7, 12), # tupla / registro
+                                (0, 8, 8),
+                                (10, -15, -5),
+                                (6, 0.75, 6.75)
+                            ]
+                        )
+
+def test_somar_dois_numeros_lista(num1, num2, resultado_esperado):
+
+    # Act / Executa
+    resultado_obtido = somar_dois_numeros(num1, num2)
+
+    # Assert / Valida
+
+    assert resultado_esperado == resultado_obtido
+
+@pytest.mark.parametrize('num1, num2, resultado_esperado',
+                            ler_csv('./fixtures/massa_somar.csv')
+                        )
+
+def test_somar_dois_numeros_csv(num1, num2, resultado_esperado):
+
+    # Padrão / Standard AAA (se diz Tiple A / 3A) = Arrange, Act, Assert
+
+    # Arrange / Prepara / Configura
+    # Dados entrada e saída fornecidos pela massa de teste em formato de lista
+
+    # Act / Executa
+    resultado_obtido = somar_dois_numeros(float(num1), float(num2))
+
+    # Assert / Valida
+    assert float(resultado_esperado) == resultado_obtido
 
